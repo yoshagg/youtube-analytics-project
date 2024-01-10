93101c22f29d0fe3ee605dac380151e7846887c2
yoshagg <smertinkirill@gmail.com>
2024-01-10 17:46:32 +0300

    hmmm

--- src/channel.py
+++ src/channel.py
@@ -1,10 +1,27 @@
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.title = title
+        self.url = url
+        self.subscribers = subscribers
+        self.video_count = video_count
+        self.view_count = view_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(self.channel_id)
+
+    def get_service(self):
+        return self
+
+    def to_json(self):
+        with open('information.json', 'a') as file:
+            file.write(f"""{self.channel.id}\n""")
+            file.write(f"""{self.title}\n""")
+            file.write(f"""{self.link}\n""")
+            file.write(f"""{self.subscribers}\n""")
+            file.write(f"""{self.video_count}\n""")
+            file.write(f"""{self.views_count}\n\n""")
