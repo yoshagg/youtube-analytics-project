3494531158e013ed25a0be0c4f4c9ed645ab7fa1
yoshagg <smertinkirill@gmail.com>
2024-02-14 16:39:54 +0300

    hmmm

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
 from googleapiclient.discovery import build
+import os
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -6,28 +7,64 @@ class Channel:
     def __init__(self, channel_id) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.title = title
-        self.url = url
-        self.subscribers = subscribers
-        self.video_count = video_count
-        self.view_count = view_count
+        self.info = self.channel_info_request()
+        self.make_attributes()
 
     def __str__(self):
-        return f'{self.title} - {self.url}'
+        return f'{self.info}'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(self.channel_id)
 
-    def get_service(self):
+    @classmethod
+    def get_service(cls):
         api_key: str = os.getenv('API_KEY')
         return build('youtube', 'v3', developerKey=api_key)
 
-    def to_json(self):
-        with open('information.json', 'a') as file:
-            file.write(f"""{self.channel.id}\n""")
+    def channel_info_request(self):
+        service = self.get_service()
+        return service.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+    def make_attributes(self):
+        info = self.channel_info_request()
+        self.title = info['items'][0]['snippet']['title']
+        self.description = info['items'][0]['snippet']['description']
+        self.url = info['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = info['items'][0]['statistics']['subscriberCount']
+        self.video_count = info['items'][0]['statistics']['videoCount']
+        self.views_count = info['items'][0]['statistics']['viewCount']
+
+    def to_json(self, doc):
+        with open(doc, 'a') as file:
+            file.write(f"""{self.channel_id}\n""")
             file.write(f"""{self.title}\n""")
-            file.write(f"""{self.link}\n""")
-            file.write(f"""{self.subscribers}\n""")
+            file.write(f"""{self.description}\n""")
+            file.write(f"""{self.url}\n""")
+            file.write(f"""{self.subscriber_count}\n""")
             file.write(f"""{self.video_count}\n""")
             file.write(f"""{self.views_count}\n\n""")
+
+
+c = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+print(c)
--- src/video.py
+++ src/video.py
@@ -1,18 +1,60 @@
+from googleapiclient.discovery import build
+import os
+import isodate
+
 class Video:
 
-    def __init__(self, id, title, url, views, likes):
-        self.id = id
-        self.title = title
-        self.url = url
-        self.views = views
-        self.likes = likes
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.info = self.video_request()
+        self.make_attributes()
 
-class PLVideo:
+    def __str__(self):
+        return f'{self.video_title}'
 
-    def __init__(self, id, title, url, views, likes, plid):
-        self.id = id
-        self.title = title
-        self.url = url
-        self.views = views
-        self.likes = likes
-        self.plid = plid
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def video_request(self):
+        service = Video.get_service()
+        return service.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                id=self.video_id
+                                                ).execute()
+
+    def make_attributes(self):
+        info = self.video_request()
+        self.video_title: str = info['items'][0]['snippet']['title']
+        self.view_count: int = info['items'][0]['statistics']['viewCount']
+        self.like_count: int = info['items'][0]['statistics']['likeCount']
+        self.comment_count: int = info['items'][0]['statistics']['commentCount']
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, pl_id):
+        self.video_id = video_id
+        self.pl_id = pl_id
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+    def playlist_request(self):
+        service = super.get_service()
+        return service.playlistItems().list(playlistId=self.pl_id,
+                                               part='contentDetails',
+                                               maxResults=50,
+                                               ).execute()
+
+    def perebor(self):
+        for video in self.playlist_request['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            print(duration)
+
+    def make_attributes(self):
+        info = self.video_request()
+        self.video_title: str = info['items'][0]['snippet']['title']
+        self.view_count: int = info['items'][0]['statistics']['viewCount']
+        self.like_count: int = info['items'][0]['statistics']['likeCount']
+        self.comment_count: int = info['items'][0]['statistics']['commentCount']
